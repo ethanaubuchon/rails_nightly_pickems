4b107bbc56d6f2766b92f4b7da5eadff3f023f07
Ethan Aubuchon <ethan.a.aubuchon@gmail.com>
2014-10-06 17:08:57 -0400

    Added scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :scores
+
+  resources :games
+
   resources :teams
 
   # The priority is based upon order of creation: first created -> highest priority.
