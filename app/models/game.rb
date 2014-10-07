2021bd272fc847cbb3b9a0c5384b00d17e42f925
Ethan Aubuchon <ethan.a.aubuchon@gmail.com>
2014-10-06 21:43:00 -0400

    Relationships

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ActiveRecord::Base
-  belongs_to :home_team
-  belongs_to :away_team
+  belongs_to :home_team, :class_name => :user, :user_id
+  belongs_to :away_team, :class_name => :user, :user_id
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+teams = JSON.parse(File.read('db/fixtures/teams.json'))
+teams.each do |team|
+  Team.create!(team)
+end
+
+game_data = JSON.parse(File.read('db/fixtures/games.json'))
+game_data.each do |data|
+  date = Time.zone.parse(data["datetime"]).utc.in_time_zone('Eastern Time (US & Canada)')
+  home_team = Team.where(short: data["home"]).first
+  away_team = Team.where(short: data["away"]).first
+  Game.create!(home_team: home_team, away_team: away_team, game_time: date)
+end
