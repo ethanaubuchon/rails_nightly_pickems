4ef59ad9db603814603b3885f0144f4e03e61207
Ethan Aubuchon <ethan.a.aubuchon@gmail.com>
2014-10-06 21:55:30 -0400

    Fixed models and seeds

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,6 +11,6 @@
 #
 
 class Game < ActiveRecord::Base
-  belongs_to :home_team, :class_name => :user, :foreign_key => "user_id"
-  belongs_to :away_team, :class_name => :user, :foreign_key => "user_id"
+  belongs_to :home_team, :class_name => :Team
+  belongs_to :away_team, :class_name => :Team
 end
