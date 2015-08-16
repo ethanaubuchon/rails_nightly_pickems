086019cc5621788c9f8cc481dae3d91e3fe326fc
Ethan Aubuchon <ethan.a.aubuchon@gmail.com>
2015-08-16 01:44:55 -0400

    Fixed the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,9 @@ count = 0
 total = game_data.count
 game_data.each do |data|
   p "#{count}/#{total} games added" if count % 10 == 0
-  p "#{count} - #{data["date"]+" "+data["time"]}" if count > 930 && count < 940
   count += 1
   date = (data["date"]+" "+data["time"]).to_datetime.change(offset: "-0400")
-  home_team = Team.where(short: data["home"]).first
-  away_team = Team.where(short: data["away"]).first
-  Game.create!(home_team: home_team, away_team: away_team, game_time: date)
+  home_team = Team.find_by(short: data["home_team"])
+  away_team = Team.find_by(short: data["away_team"])
+  Game.create!(home_team_id: home_team.id, away_team_id: away_team.id, game_time: date)
 end
