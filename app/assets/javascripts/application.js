1ccd4a757945aea80792c5a7e77122e9efe8adee
Ethan Aubuchon <ethan.a.aubuchon@gmail.com>
2015-08-16 00:26:14 -0400

    Cleaned up assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require jQuery/dist/jquery
 //= require turbolinks
 //= require_tree .
