9417aa99f713dd628674431f8fec8cafba490531
Ethan Aubuchon <ethan.a.aubuchon@gmail.com>
2015-09-27 11:45:45 -0400

    Updated deployment details

--- db/migrate/20150823162714_update_pick_fields.rb
+++ db/migrate/20150823162714_update_pick_fields.rb
@@ -1,7 +1,7 @@
 class UpdatePickFields < ActiveRecord::Migration
   def change
-    remove_column :picks, :game
-    remove_column :picks, :team
+#    remove_column :picks, :game
+#    remove_column :picks, :team
     add_reference :picks, :game_teams, index: true
   end
 end
